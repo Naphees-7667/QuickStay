e806c6fb02777eb992ea382ff5d89660d034ff08
Naphees <naphees6200@gmail.com>
2025-06-07 11:15:28 +0530

    MyBooking History Page Completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import Home from './Pages/Home';
 import Footer from './components/Footer';
 import AllRooms from './Pages/AllRooms';
 import RoomDetails from './Pages/RoomDetails';
+import MyBookings from './Pages/MyBookings';
 
 const App = () => {
 
@@ -18,6 +19,7 @@ const App = () => {
           <Route  path="/" element={<Home />} />
           <Route  path="/rooms" element={<AllRooms />} />
           <Route  path="/rooms/:id" element={<RoomDetails />} />
+          <Route  path="/my-bookings" element={<MyBookings />} />
         </Routes>
       </div>
       <Footer />
